96b5dfe9f7fcf992a2618c843852a2a74a9ca189
Gustavo Felix <gustavo.felix@alvorsoftware.com>
2025-12-23 13:56:28 +0200

    test(api and domain): add basic EventDomain test, add WebApplicationFactory smoke tests for events

---
+++ tests/Calendar.Tests/Domain/EventDomainTests.cs
@@ -0,0 +1,31 @@
+﻿using Calendar.Domain.Common;
+using Calendar.Domain.Events;
+using FluentAssertions;
+
+namespace Calendar.Tests.Domain;
+
+public class EventDomainTests
+{
+    [Fact]
+    public void CreateEvent_EndBeforeStart_ShouldThrow()
+    {
+        var start = DateTimeOffset.UtcNow;
+        var end = start.AddMinutes(-1);
+
+        Action act = () => new CalendarEvent("Title", null, start, end);
+        act.Should().Throw<DomainException>().WithMessage("*EndTime must be after StartTime*");
+    }
+
+    [Fact]
+    public void AddAttendee_DuplicateEmail_ShouldThrow()
+    {
+        var start = DateTimeOffset.UtcNow;
+        var end = start.AddHours(1);
+        var ev = new CalendarEvent("Title", null, start, end);
+
+        ev.AddAttendee(new Attendee("A", "a@test.com"));
+
+        Action act = () => ev.AddAttendee(new Attendee("B", "a@test.com"));
+        act.Should().Throw<DomainException>().WithMessage("*unique*");
+    }
+}
